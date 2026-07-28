777d732aa93865f548ce38708812b99cd2a6deb6
JackNiu <goodnc@163.com>
2026-07-28 17:42:00 +0800

    Add mongodb connection model.

---
+++ manage-backend/config/models/conn.ts
@@ -0,0 +1,15 @@
+// 引入mongoose第三方模块
+import mongoose from "mongoose";
+import config from "config";
+// 连接数据库
+export function openConnectDb() {
+  const url = `mongodb://${config.get("db.user")}:${config.get("db.pwd")}@${config.get("db.host")}:${config.get("db.port")}/${config.get("db.name")}`;
+  mongoose
+    .connect(url)
+    .then(() => {
+      console.log("数据库连接成功");
+    })
+    .catch((err) => {
+      console.error("数据库连接失败", err);
+    });
+}

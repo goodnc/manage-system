ebe56d819475b00d906b2240aed28f2c8d22d14c
JackNiu <goodnc@163.com>
2026-07-29 17:53:11 +0800

    Fixed bug

--- manage-frontend/src/main.ts
+++ manage-frontend/src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
-import pinia from "./store/index.js";
+import pinia from "./store/index";
 import "./style.css";
 import App from "./App.vue";
 const app = createApp(App);

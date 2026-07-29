650491108f3fd590951e785dea80217d16159463
JackNiu <goodnc@163.com>
2026-07-29 17:19:03 +0800

    Add pinia-plugin-persistedstate plugin.

--- manage-frontend/src/store/index.ts
+++ manage-frontend/src/store/index.ts
@@ -1,5 +1,8 @@
 import { createPinia } from "pinia";
+import piniaPluginPersistedState from "pinia-plugin-persistedstate";
 // 创建
 const pinia = createPinia();
+// 固化
+pinia.use(piniaPluginPersistedState);
 // 导出
 export default pinia;

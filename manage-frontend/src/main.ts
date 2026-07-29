e6a47fa50e86c4f7b29ed3c3581f5c2c48ea3725
JackNiu <goodnc@163.com>
2026-07-29 17:24:21 +0800

    Use pinia in main.ts

--- manage-frontend/src/main.ts
+++ manage-frontend/src/main.ts
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+import { createApp } from "vue";
+import pinia from "./store/index.js";
+import "./style.css";
+import App from "./App.vue";
+const app = createApp(App);
+app.use(pinia);
+app.mount("#app");

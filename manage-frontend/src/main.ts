b97681276e6b6a4ad2eb3edfea7604dd8c418d6e
JackNiu <goodnc@163.com>
2026-07-30 16:45:07 +0800

    Add ant-design-vue css.

--- manage-frontend/src/main.ts
+++ manage-frontend/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import pinia from "./store/index";
+import "ant-design-vue/dist/antd.css";
 import "./style.css";
 import App from "./App.vue";
 const app = createApp(App);

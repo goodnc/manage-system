9b1db8792efc395b4a36d69dddf3d785b98e69d1
JackNiu <goodnc@163.com>
2026-08-01 23:52:55 +0800

    Add breadcrumb component.

---
+++ manage-frontend/src/components/nav/Breadcrumb.vue
@@ -0,0 +1,17 @@
+<template>
+  <a-breadcrumb style="margin: 16px 0">
+    <a-breadcrumb-item v-for="(item, index) in breadcrumbData">
+      <span class="no-link" v-if="index === breadcrumbData.length - 1">{{
+        item.meta.title
+      }}</span>
+      <router-link v-else :to="item.path">{{ item.meta.title }}</router-link>
+    </a-breadcrumb-item>
+  </a-breadcrumb>
+</template>
+<script setup lang="ts">
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+const breadcrumbData = computed(() => {
+  return useRoute().matched.filter((f: any) => f.meta && f.meta.title);
+});
+</script>

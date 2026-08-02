59cf6b783de0747565ea07eb16c13272366356c9
JackNiu <goodnc@163.com>
2026-08-02 15:52:36 +0800

    Update layout index.

--- manage-frontend/src/components/layout/index.vue
+++ manage-frontend/src/components/layout/index.vue
@@ -1,9 +1,29 @@
 <template>
   <a-layout>
     <Header></Header>
+    <a-layout>
+      <Sider></Sider>
+      <a-layout style="padding: 0 24px 10px">
+        <Breadcrumb></Breadcrumb>
+        <a-layout-content>
+          <router-view></router-view>
+        </a-layout-content>
+      </a-layout>
+    </a-layout>
   </a-layout>
 </template>
 
-<script>
-import Header from "./components/header/Header.vue";
+<script lang="ts" setup>
+import Breadcrumb from "../nav/Breadcrumb.vue";
+import Header from "./header/Header.vue";
+import Sider from "./sider/Sider.vue";
 </script>
+
+<style lang="scss" scoped>
+.ant-layout-content {
+  height: calc(100vh-128px);
+  background: #fff;
+  padding: 24px;
+  margin: 0;
+}
+</style>

9e647a9838529ab4c81dfc56fc692f3a8f61c38d
JackNiu <goodnc@163.com>
2026-08-04 23:29:01 +0800

    Update home template.

--- manage-frontend/src/views/home/home.vue
+++ manage-frontend/src/views/home/home.vue
@@ -1,2 +1,49 @@
-<template>首页</template>
+<template>
+  <div class="main">
+    <a-row :gutter="25">
+      <a-col :span="8">
+        <div class="cell s1">
+          <i class="iconfont icon-yonghu"></i>
+          <h4>登录用户</h4>
+          <h5></h5>
+        </div>
+      </a-col>
+      <a-col :span="8">
+        <div class="cell s2">
+          <i class="iconfont icon-zhuce"></i>
+          <h4>新增注册</h4>
+          <h5></h5>
+        </div>
+      </a-col>
+      <a-col :span="8">
+        <div class="cell s3">
+          <i class="iconfont icon-xinzeng"></i>
+          <h4>课程新增学员</h4>
+          <h5></h5>
+        </div>
+      </a-col>
+      <a-col :span="8">
+        <div class="cell s4">
+          <i class="iconfont icon-banjiguanli"></i>
+          <h4>班级新增学员</h4>
+          <h5></h5>
+        </div>
+      </a-col>
+      <a-col :span="8">
+        <div class="cell s5">
+          <i class="iconfont icon-huiyuan"></i>
+          <h4>新增会员</h4>
+          <h5></h5>
+        </div>
+      </a-col>
+      <a-col :span="8">
+        <div class="cell s6">
+          <i class="iconfont icon-yiwen"></i>
+          <h4>未回复问答</h4>
+          <h5></h5>
+        </div>
+      </a-col>
+    </a-row>
+  </div>
+</template>
 <script lang="ts" setup></script>

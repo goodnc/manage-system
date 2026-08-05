bb542a80485ba9aec0471201b3c65f2077b77765
JackNiu <goodnc@163.com>
2026-08-05 21:08:54 +0800

    Update home template.

--- manage-frontend/src/views/home/home.vue
+++ manage-frontend/src/views/home/home.vue
@@ -5,42 +5,42 @@
         <div class="cell s1">
           <i class="iconfont icon-yonghu"></i>
           <h4>登录用户</h4>
-          <h5></h5>
+          <h5>{{ login_user }}</h5>
         </div>
       </a-col>
       <a-col :span="8">
         <div class="cell s2">
           <i class="iconfont icon-zhuce"></i>
           <h4>新增注册</h4>
-          <h5></h5>
+          <h5>{{ new_register }}</h5>
         </div>
       </a-col>
       <a-col :span="8">
         <div class="cell s3">
           <i class="iconfont icon-xinzeng"></i>
           <h4>课程新增学员</h4>
-          <h5></h5>
+          <h5>{{ new_stu_course }}</h5>
         </div>
       </a-col>
       <a-col :span="8">
         <div class="cell s4">
           <i class="iconfont icon-banjiguanli"></i>
           <h4>班级新增学员</h4>
-          <h5></h5>
+          <h5>{{ new_stu_classes }}</h5>
         </div>
       </a-col>
       <a-col :span="8">
         <div class="cell s5">
           <i class="iconfont icon-huiyuan"></i>
           <h4>新增会员</h4>
-          <h5></h5>
+          <h5>{{ new_member }}</h5>
         </div>
       </a-col>
       <a-col :span="8">
         <div class="cell s6">
           <i class="iconfont icon-yiwen"></i>
           <h4>未回复问答</h4>
-          <h5></h5>
+          <h5>{{ not_reply }}</h5>
         </div>
       </a-col>
     </a-row>
@@ -55,7 +55,7 @@ const state = reactive<any>({
   new_stu_course: 0,
   new_stu_classes: 0,
   new_member: 0,
-  new_reply: 0,
+  not_reply: 0,
 });
 const initData = () => {
   getHomeData().then((res: any) => {
@@ -63,7 +63,7 @@ const initData = () => {
       login_user,
       new_member,
       new_register,
-      new_reply,
+      not_reply,
       new_stu_classes,
       new_stu_course,
     } = res.data;
@@ -72,7 +72,7 @@ const initData = () => {
     state.new_stu_course = new_stu_course;
     state.new_stu_classes = new_stu_classes;
     state.new_member = new_member;
-    state.new_reply = new_reply;
+    state.not_reply = not_reply;
   });
 };
 
@@ -83,6 +83,6 @@ const {
   new_stu_course,
   new_stu_classes,
   new_member,
-  new_reply,
+  not_reply,
 } = toRefs(state);
 </script>

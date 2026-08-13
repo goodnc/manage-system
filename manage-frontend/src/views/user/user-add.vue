d3ab2ac45ce1e41fb8c26b3abbb00e519df59efb
JackNiu <goodnc@163.com>
2026-08-13 23:58:06 +0800

    Add user add component.

---
+++ manage-frontend/src/views/user/user-add.vue
@@ -0,0 +1,13 @@
+<template>
+  <a-modal
+    :title="state.winTitle"
+    :open="state.winVisible"
+    @ok="handleOk"
+    :confirmLoading="state.winLoading"
+    @cancel="handleCancel"
+    cancelText="取消"
+    okText="取消"
+    okText="确定">
+  </a-modal>
+</template>
+<script setup lang="ts"></script>
